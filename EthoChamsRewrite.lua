e4cca492f0aa34a88b9e893323049f8a512668a3
Etho <72043735+Etho414@users.noreply.github.com>
2022-12-26 16:07:42 -0700

    Create EthoChamsRewrite.lua

---
+++ EthoChamsRewrite.lua
@@ -0,0 +1,88 @@
+local ReturnTabThing = {}
+local player = game.Players.LocalPlayer
+local HighlightFold = Instance.new("Folder",game.CoreGui)
+local StopHighlight = false
+local ButtonPressServ;
+local PlayerAddServ;
+
+_G.StopGlobalEthoChams = false
+
+if _G.RanEthoChams == true then
+    _G.StopGlobalEthoChams = true
+    wait(1)
+    _G.StopGlobalEthoChams = false
+end
+
+_G.RanEthoChams = true 
+
+function Cham(v,SettingsTab)
+    local RunServ;
+    local PlayerRemoveServ;
+    local HighLight = Instance.new("Highlight",HighlightFold)
+
+    local function Stop()
+        PlayerRemoveServ:Disconnect()
+        RunServ:Disconnect() 
+        HighLight:Destroy()
+        PlayerAddServ:Disconnect()
+        if _G.StopGlobalEthoChams == true then
+            ButtonPressServ:Disconnect()
+
+        end
+    end
+    RunServ = game:GetService("RunService").RenderStepped:Connect(function(deltaTime)
+        pcall(function()
+            HighLight.Adornee = v.Character
+            HighLight.Enabled = not StopHighlight
+            HighLight.FillTransparency = _G[SettingsTab.FillGlobal]
+            HighLight.OutlineTransparency = _G[SettingsTab.OutlineGlobal]
+        end)
+        if _G.StopGlobalEthoChams == true then Stop() end
+    end)
+    
+    PlayerRemoveServ = game:GetService("RunService").RenderStepped:connect(function(v2)
+        if v2 == v then
+            Stop()
+        end
+    end)
+end
+function ToggleCham(TogVal,SettingsTab)
+    if TogVal == true then
+        StopHighlight = false
+    else
+        StopHighlight = true
+    end
+end
+
+
+
+
+
+
+function ReturnTabThing:InitChams(SettingsTab,ButtonToggle)
+    local ChamsToggle = true
+    ButtonToggle = string.upper(ButtonToggle)
+    ToggleCham(ChamsToggle,SettingsTab)
+    for i,v in pairs(game.Players:GetChildren()) do 
+        if v ~= player then   
+            Cham(v,SettingsTab)
+        end
+    end
+    PlayerAddServ = game.Players.PlayerAdded:connect(function(v)
+        if v ~= player then
+            Cham(v,SettingsTab)
+        end
+    end)
+    ButtonPressServ = game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessedEvent)
+        if gameProcessedEvent then return end
+        if input.KeyCode == Enum.KeyCode[ButtonToggle] then
+            ChamsToggle = not ChamsToggle
+            ToggleCham(ChamsToggle,SettingsTab)
+        end
+    end)
+end
+
+
+
+
+return ReturnTabThing
