df6a439a2ad0e2107e2390684d54f3f6a35d98ed
Etho <72043735+Etho414@users.noreply.github.com>
2023-01-06 04:10:37 -0700

    Update EspBaseRewrite!.lua

--- EspBaseRewrite!.lua
+++ EspBaseRewrite!.lua
@@ -131,12 +131,13 @@ function ESPFunctionReturnTable:RefreshHighlight(PassedTable,PartToAdornee)
             PassedTable.Data.Highlight.Refreshing = true 
             local HighlightObj = PassedTable.Data.Highlight.HighlightObj
             local HighlightObj2 = PassedTable.Data.Highlight.HighlightObj2
+            HighlightObj2.Adornee = nil
             HighlightObj2.Adornee = PartToAdornee
-            wait(0.3)
+            wait(1)
             HighlightObj.Adornee = nil
-            wait(0.3)
+            wait(1)
             HighlightObj.Adornee = PartToAdornee
-            wait(0.3)
+            wait(1)
             HighlightObj2.Adornee = nil
             PassedTable.Data.Highlight.Refreshing = false 
         end
