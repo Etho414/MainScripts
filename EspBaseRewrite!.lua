27de9538912ef2243d84d8ebdaf420008b3255e6
Etho <72043735+Etho414@users.noreply.github.com>
2023-01-09 03:06:27 -0700

    Update EspBaseRewrite!.lua

--- EspBaseRewrite!.lua
+++ EspBaseRewrite!.lua
@@ -307,8 +307,8 @@ function ESPRenderer()
             table.remove(ESPListenTable,i)
         elseif DrawESP == false then
             VisibleText(false, OptionTable)
-        elseif not (_G[OptionTable.GlobalVariableTable.TextToggle] == true and _G[OptionTable.GlobalVariableTable.BoxToggle] == true and _G[OptionTable.GlobalVariableTable.HpBarToggle] == true and  _G[OptionTable.GlobalVariableTable.ChamsToggle] == true) then
-            VisibleText(false,OptionTable)
+        --[[elseif not (_G[OptionTable.GlobalVariableTable.TextToggle] == true and _G[OptionTable.GlobalVariableTable.BoxToggle] == true and _G[OptionTable.GlobalVariableTable.HpBarToggle] == true and  _G[OptionTable.GlobalVariableTable.ChamsToggle] == true) then
+            VisibleText(false,OptionTable)]]
         elseif PauseRender == false then
             local PositionCached = OptionTable.Data.ReturnPosFunc(OptionTable) 
    
